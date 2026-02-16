5f4dbbe26508496c93ceacb89a2254264328cebd
Prince Pandey <princepandey5432@gmail.com>
2026-02-16 19:28:00 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "prince@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed
+
+accountEmail = "pp@pp.com"
+accountPasword = "21212121"
+accountCity = "Bengaluru"
+let accountState;
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
